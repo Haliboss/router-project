81b3fe402416687413d5495148e2472f9777aa2f
Haliboss <halibos@gmail.com>
2022-11-07 09:41:26 +0300

    go back go home

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
 
+  const navigate = useNavigate();
   const {id} = useParams();
   console.log(id)
+
+  const {state: person} = useLocation();
+  console.log(person);
   
-  return <div>PersonDetail</div>;
+  return (
+    <div className="container text-center">
+      <h3>{person.first_name}</h3>
+      <img src={person.avatar} className="rounded" alt="img" />
+      <p>{person.email}</p>
+      <div>
+        <button className="btn btn-success me-2" onClick={() => navigate("/")}>
+          Go Home
+        </button>
+        <button className="btn btn-warning" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
