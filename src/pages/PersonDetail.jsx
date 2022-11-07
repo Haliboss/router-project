f589914c6e6feecd2c9f1f917bf39d7b37d5ba67
Haliboss <halibos@gmail.com>
2022-11-07 10:11:29 +0300

    finished

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,20 +1,32 @@
-import React from "react";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
-
   const navigate = useNavigate();
-  const {id} = useParams();
-  console.log(id)
+  const { id } = useParams();
+  //console.log(id);
 
-  const {state: person} = useLocation();
+  // const {state: person} = useLocation();
+  // console.log(person);
+
+  const [person, setPerson] = useState("");
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPerson(data.data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
   console.log(person);
-  
+
   return (
     <div className="container text-center">
-      <h3>{person.first_name}</h3>
-      <img src={person.avatar} className="rounded" alt="img" />
-      <p>{person.email}</p>
+      <h3>
+        {person?.first_name} {person?.last_name}
+      </h3>
+      <img src={person?.avatar} className="rounded" alt="" />
+      <p>{person?.email}</p>
       <div>
         <button className="btn btn-success me-2" onClick={() => navigate("/")}>
           Go Home
