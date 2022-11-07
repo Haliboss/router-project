66ded97295825edfa2f6237160f8542b326e3a2e
Haliboss <halibos@gmail.com>
2022-11-08 01:45:00 +0300

    add navigate

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const PersonDetail = () => {
   const navigate = useNavigate();
@@ -10,33 +11,47 @@ const PersonDetail = () => {
   // console.log(person);
 
   const [person, setPerson] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          setError(true);
+          throw new Error("Something went wrong");
+        }
+        return res.json();
+      })
       .then((data) => setPerson(data.data))
       .catch((err) => console.log(err));
-  }, [id]);
+  }, []);
 
   console.log(person);
 
-  return (
-    <div className="container text-center">
-      <h3>
-        {person?.first_name} {person?.last_name}
-      </h3>
-      <img src={person?.avatar} className="rounded" alt="" />
-      <p>{person?.email}</p>
-      <div>
-        <button className="btn btn-success me-2" onClick={() => navigate("/")}>
-          Go Home
-        </button>
-        <button className="btn btn-warning" onClick={() => navigate(-1)}>
-          Go Back
-        </button>
+  if (error) {
+    return <NotFound />;
+  } else {
+    return (
+      <div className="container text-center">
+        <h3>
+          {person?.first_name} {person?.last_name}
+        </h3>
+        <img src={person?.avatar} className="rounded" alt="" />
+        <p>{person?.email}</p>
+        <div>
+          <button
+            className="btn btn-success me-2"
+            onClick={() => navigate("/")}
+          >
+            Go Home
+          </button>
+          <button className="btn btn-warning" onClick={() => navigate(-1)}>
+            Go Back
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PersonDetail;
